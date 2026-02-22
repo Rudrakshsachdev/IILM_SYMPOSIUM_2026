d2eca993dab3e9ad2af3a8c1182f874e96b2c0a6
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-02-22 22:28:19 +0530

    Added CallForPapers Section

--- iilm-symposium/src/App.jsx
+++ iilm-symposium/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About/About";
 import ImportantDates from "./components/ImportantDates/ImportantDates";
 import Speakers from "./components/Speakers/Speakers";
 import Organizers from "./components/Organizers/Organizers";
+import CallForPapers from "./components/CallForPapers/CallForPapers";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <ImportantDates />
       <Speakers />
       <Organizers />
+      <CallForPapers />
     </>
   );
 }
