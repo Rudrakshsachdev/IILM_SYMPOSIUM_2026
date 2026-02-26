2a4ecde7d32ee158cb41a76ef524fae5dddb9fdd
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-02-26 10:58:26 +0530

    feat: updated the cmt section for verification

--- iilm-symposium/src/components/Acknowledgement/Acknowledgement.jsx
+++ iilm-symposium/src/components/Acknowledgement/Acknowledgement.jsx
@@ -82,9 +82,9 @@ const Acknowledgement = () => {
                         <p className={styles.quoteText}>
                             The Microsoft CMT service was used for managing the peer-reviewing
                             process for this conference. This service was provided for free by
-                            Microsoft and they bore all expenses, including costs for{" "}
-                            <strong>Azure cloud services</strong> as well as for{" "}
-                            <strong>software development and support</strong>.
+                            Microsoft and they bore all expenses, including costs for
+                            Azure cloud services as well as for
+                            software development and support.
                         </p>
                     </blockquote>
 
